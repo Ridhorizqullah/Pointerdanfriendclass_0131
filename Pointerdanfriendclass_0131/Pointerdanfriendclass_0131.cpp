91fe83092240966b78153485a0bcc08cb8823efa
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-06 10:51:02 +0700

    pointer

---
+++ Pointerdanfriendclass_0131/Pointerdanfriendclass_0131.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim() {
+		cout << "No induk = " <<nim<<endl;
+	}
+
+};
+
+int main() {
+	mahasiswa mhs{1}; //object mhs
+	mhs.showNim(); //member access operator
+	
+	mahasiswa& refMhs = mhs; //pointer reference refMhs
+	refMhs.nim = 2;
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer dereference pmhs
+	pMhs->nim = 3; //arrow operator
+	mhs.showNim();
+	return 0;
+}
